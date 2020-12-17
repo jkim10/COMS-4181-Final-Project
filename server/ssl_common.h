79330b3f808c2c141bd8d883c9cfe3b9fbada82c
GitZChen <18753200+GitZChen@users.noreply.github.com>
2020-12-18 00:38:27 +0800

    Mid-point checkin

---
+++ server/ssl_common.h
@@ -0,0 +1,64 @@
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+
+namespace my {
+
+template<class T> struct DeleterOf;
+template<> struct DeleterOf<BIO> { void operator()(BIO *p) const { BIO_free_all(p); } };
+template<> struct DeleterOf<BIO_METHOD> { void operator()(BIO_METHOD *p) const { BIO_meth_free(p); } };
+template<> struct DeleterOf<SSL_CTX> { void operator()(SSL_CTX *p) const { SSL_CTX_free(p); } };
+
+template<class OpenSSLType>
+using UniquePtr = std::unique_ptr<OpenSSLType, DeleterOf<OpenSSLType>>;
+
+my::UniquePtr<BIO> operator|(my::UniquePtr<BIO> lower, my::UniquePtr<BIO> upper)
+{
+    BIO_push(upper.get(), lower.release());
+    return upper;
+}
+
+class StringBIO {
+    std::string str_;
+    my::UniquePtr<BIO_METHOD> methods_;
+    my::UniquePtr<BIO> bio_;
+public:
+    StringBIO(StringBIO&&) = delete;
+    StringBIO& operator=(StringBIO&&) = delete;
+
+    explicit StringBIO() {
+        methods_.reset(BIO_meth_new(BIO_TYPE_SOURCE_SINK, "StringBIO"));
+        if (methods_ == nullptr) {
+            throw std::runtime_error("StringBIO: error in BIO_meth_new");
+        }
+        BIO_meth_set_write(methods_.get(), [](BIO *bio, const char *data, int len) -> int {
+            std::string *str = reinterpret_cast<std::string*>(BIO_get_data(bio));
+            str->append(data, len);
+            return len;
+        });
+        bio_.reset(BIO_new(methods_.get()));
+        if (bio_ == nullptr) {
+            throw std::runtime_error("StringBIO: error in BIO_new");
+        }
+        BIO_set_data(bio_.get(), &str_);
+        BIO_set_init(bio_.get(), 1);
+    }
+    BIO *bio() { return bio_.get(); }
+    std::string str() && { return std::move(str_); }
+};
+
+[[noreturn]] void print_errors_and_exit(const char *message)
+{
+    fprintf(stderr, "%s\n", message);
+    ERR_print_errors_fp(stderr);
+    exit(1);
+}
+
+[[noreturn]] void print_errors_and_throw(const char *message)
+{
+    my::StringBIO bio;
+    ERR_print_errors(bio.bio());
+    throw std::runtime_error(std::string(message) + "\n" + std::move(bio).str());
+}
+
+}
