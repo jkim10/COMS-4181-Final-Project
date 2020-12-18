8dea308bbf602ee28915fc352a9be1b7519d3b16
abzjy024 <zjynglll@163.com>
2020-12-18 17:08:31 -0500

    cp utils from sendmsg_recvmsg_client to server

---
+++ server/utils_server.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <dirent.h>
+#include <algorithm>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <vector>
+
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+
+using namespace std;
+
+string ReadFiletoString(const char *filename);
+
+void WriteStringtoFile(string file, string filename);
+
+string GetCert(string recipient);
+
+bool VerifyCert(string client_cert);
+
+void UploadMessage(string message, string recipient);
+
+string GetMessage(string recipient);
+
+string ParseSendmsg(string content, vector<string> &recipients);
+
+string CertstoSend(string client_cert, vector<string> recipients);
