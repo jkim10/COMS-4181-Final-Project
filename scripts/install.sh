656163a7e8d165f38b9d54d482dff9f0fbfe305f
GitZChen <18753200+GitZChen@users.noreply.github.com>
2020-12-19 13:44:11 -0500

    fixed some install.sh bug

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 1 ]; then
     echo "Usage: ./install.sh <install_dir>"
     exit 1
 fi
+
+set -e
+
+function get_rand() {
+	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1
+}
+
+rca_pass="$(get_rand)"
+ica_pass="$(get_rand)"
+server_pass="$(get_rand)"
+client_pass="$(get_rand)"
 
 rm -rf $1
 mkdir -p $1
@@ -28,6 +39,8 @@ pushd $1
 ./create-tree.sh
 rm create-tree.sh
 popd
+
+set +e
 
 echo -e "\n"
 echo -e "Installation finished successfully!"
